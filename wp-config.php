61c2c546e17ad87b497e2a67ea826427fc5815e3
Neetesh30 <neetesh652@gmail.com>
2020-05-08 13:42:58 +0530

    add finance wp web config7

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'cBqK9FoZ5l' );
 define( 'DB_PASSWORD', '7qM3huweqN' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'remotemysql.com/' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
